c44546d8c9069db5a69d47ccac68215249206d06
VikarantShrestha <vikarant0508@gmail.com>
2025-03-08 21:17:29 +0530

    second commit

--- ADT.py
+++ ADT.py
@@ -1,22 +1,19 @@
-class adt:
-    def __init__(self, arr):
-        self.arr=arr
-
-    def getElByIndx(self,i):
-        print(self.arr[i])
+class Array1:
+    def __init__(self, max_size):
+        self.l = []
+        self.max_size = max_size
     
-    def update_El_by_i(self,i,n):
-        self.arr[i]=n
+    def CreateArray(self):
+        print(f"Enter {self.max_size} elements:")
+        for i in range(self.max_size):
+            element = input(f" Enter element {i + 1}: ")
+            self.l.append(element)
     
-    def showAll(self):
-        print(self.arr)
+    def display(self):
+        print(self.l)
 
-    def getMax(self):
-        print(max(self.arr))
 
-arr=[2,4,6,8]
-ob=adt(arr)
-ob.getElByIndx(1)
-ob.update_El_by_i(1,8)
-ob.showAll()
-ob.getMax()
+max_size = int(input("Enter max size of array: "))
+ob = Array1(max_size)
+ob.CreateArray()
+ob.display()
